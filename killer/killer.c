6f26f1cf7115f2a37bfed8e23b808236fcf6a293
Kevin Hernández Rostrán <kevinah95@gmail.com>
2020-02-27 21:09:03 +0000

    Initial commit :tada:

---
+++ killer/killer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h> // math for exponential function
+#include <time.h> // time for seeding
+#include <pthread.h> // include pthread functions and structures
+#include <semaphore.h> // include semaphores
+#include <unistd.h> // include sleep
+
+int main(int argc, char *argv[]) {
+  printf("Killer...");
+
+  sleep(1);
+  exit(0);
+}
