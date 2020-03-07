09159ab44bfab1e925cccf25adfc9c44d7b96a72
elisa7143 <eli0890@gmail.com>
2020-03-06 19:37:26 -0600

    c modificado

--- creator/creator.c
+++ creator/creator.c
@@ -16,10 +16,17 @@ static int consumers_counter = 0;
 static int producer_flag = 0; //when a process is being produced.
 static int consumer_flag = 0; //when a process is being consumed.
 static int input_message_size = 0;
-sem_t sem1;
+static int buffer_size;
+static circular_buf_t buffer;
+static sem_t sem1;
 
 int main(int argc, char *argv[]) {
     printf("Creador... \n");
+    printf("Digite el tamaño del buffer \n");
+    scanf("%d", &buffer_size);
+    //Definición del tamaño del buffer
+    buffer.max = buffer_size;
+
     //shared semaphore
     sem_init(&sem1, 1, 1);
 
