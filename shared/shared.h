f5ed5a76312713caf3f2c386c8b88f8075bdb454
Jose Pablo <araya.josepablo@gmail.com>
2020-03-15 11:36:04 -0600

    SOA Implementing consumer.c logic

--- shared/shared.h
+++ shared/shared.h
@@ -7,20 +7,30 @@
 #include <errno.h>
 
 static const char * producers_mem_name = "producers_mem_name";
-static const char * consumers_mem_name = "consumers_mem_name";
+
+static const char * consumers_mem_name = "/CONSUMERS_SHARED_MEMORY";
+
 static const char * fill_sem_name= "fill";
 static const char * avail_sem_name= "avail";
 static const char * mutex_sem_name= "mutex";
+
 const char * NAME_MEMORY_SUSPEND = "SHARED_MEMORY_SUSPEND";
+
 static int buffer_shm_fd;   //shared memory file discriptor
 static int producers_shm_fd;
 static int consumers_shm_fd;
+
 static int SHAREDM_FILEDESCRIPTOR_SUSPEND;   //shared memory file discriptor for suspend process
+
 static struct circular_buf_t *buffer_mem_ptr;
+
 static int * producers_mem_ptr;
 static int * consumers_mem_ptr;
 static int * SUSPEND; //If true suspend the process
+
 static sem_t * fill_sem, * avail_sem, * mutex_sem;
 
+const char special_message[] = "KILLER_MESSAGE_FROM_FINISHER"; // Special message that indicates the process must finish
 
-
+static int default_buffer_size = 10;
+char default_buffer_name[] = "shared_memory";
